86b2f3a51779575dccbc75aa73a49511920ab920
Jana <45703020+janadev1@users.noreply.github.com>
2023-12-05 13:44:48 +0100

    Testing basic structure

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# dotfiles
-My own dotfiles.
+Notes taken from the following examples:
+
+- CodelyTV/dotly
+- ...
+
+To simplify the process as much as possible the user should have the option
+to `wget` an installation script instead of having to clone the whole repo
+manually.
+
+If a package needed for installation is not found, user should be given the
+option of either telling the script to install it or stop the installation and
+do it manually.
+
+Script should contemplate different package managers for different
+distros (apt, pacman, etc).
+
+Script should contain a way of installing any necessary software, even if not
+in the package manager. For example, Ubuntu's Neovim repos suck, script should
+be able to download lastest stable (or user given) version and install it properly, while
+also giving information of what it is doing.
+
+Information shown to the user should be abundant: make what the script is doing clear.
+
+(Optional) The CLI should be pretty: add some colors!
--- install.sh
+++ install.sh
@@ -1,13 +1,43 @@
 #!/usr/bin/env bash
 
-echo "This is a test dotfiles installer"
+set -e
+
+cd "$(dirname "$0")"
+DOTFILES_ROOT=$(pwd -P)
+
+# Taken from CodelyTV/dotly
+# red='\033[0;31m'
+# green='\033[0;32m'
+# purple='\033[0;35m'
+# normal='\033[0m'
+
+# Taken from holman/dotfiles
+info() {
+	printf "\r  [ \033[00;34m..\033[0m ] %s\n" "$1"
+}
+
+user() {
+	printf "\r  [ \033[0;33m??\033[0m ] %s\n" "$1"
+}
+
+success() {
+	printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n" "$1"
+}
+
+fail() {
+	printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n" "$1"
+	echo ''
+	exit
+}
+
+info "This is a test dotfiles installer"
 
 # This test consists of installing my nvim configuration
 # from Github. Just that
 #
 # 1. Check if an nvim config is already present, and if so, back it up.
 if [ -d "$HOME/.config/nvim" ]; then
-	echo "Neovim configuration found, backing up..."
+	info "Neovim configuration found, backing up..."
 	mv "$HOME/.config/nvim/" "$HOME/.config/nvim.bak/"
 fi
 
@@ -20,6 +50,7 @@ if test "$(which nvim)"; then
 
 	if ! test "$(which wget)"; then
 		echo "wget isn't installed, can't download Neovim tarball."
+		exit
 	fi
 
 	wget "https://github.com/neovim/neovim/releases/download/v${version:-0.9.4}/nvim-linux64.tar.gz"
